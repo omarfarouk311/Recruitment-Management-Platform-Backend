781d8ab8320d1f66e9bf37c7106f058ad14fe9e6
Omar <omarfarouk219@gmail.com>
2025-01-28 23:40:06 +0200

    Configure Kafka instances

---
+++ Kafka/init.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Wait for Kafka brokers to be ready
+while ! kafka-broker-api-versions.sh --bootstrap-server kafka1:9092; do
+sleep 2;
+done;
+
+while ! kafka-broker-api-versions.sh --bootstrap-server kafka2:9092; do
+sleep 2;
+done;
+
+# List of topics to create
+topics=(
+"cv_parsing"
+"job_parsing"
+"cv_embedding_generation"
+"job_embedding_generation"
+"profile_embedding_generation"
+"logs"
+)
+
+# Function to check if a topic exists
+topic_exists() {
+kafka-topics.sh --list --bootstrap-server kafka1:9092 | grep -wq "$1"
+}
+
+# Create topics if they do not exist
+for topic in "${topics[@]}"; do
+if topic_exists "$topic"; then
+    echo "Topic '$topic' already exists."
+else
+    kafka-topics.sh --create --bootstrap-server kafka1:9092 --partitions 1 --topic "$topic"
+    echo "Topic '$topic' created."
+fi
+done;
