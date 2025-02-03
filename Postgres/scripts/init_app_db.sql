bc1a1908e714b1da034f5e6cd6ccbd7659981219
Ali <2021170338@cis.asu.edu.eg>
2025-02-03 03:53:24 +0200

    Refactor database schema: change primary key types, update foreign key constraints, and optimize indexes

--- Postgres/scripts/init_app_db.sql
+++ Postgres/scripts/init_app_db.sql
@@ -9,7 +9,7 @@ CREATE TABLE Users (
 );
 
 CREATE TABLE Job_Seeker (
-  id serial PRIMARY KEY,
+  id int PRIMARY KEY,
   phone_number TEXT NOT NULL UNIQUE,
   date_of_birth DATE NOT NULL,
   gender BOOLEAN NOT NULL,
@@ -19,19 +19,19 @@ CREATE TABLE Job_Seeker (
 );
 
 CREATE TABLE Recruiter (
-  id serial PRIMARY KEY ,
+  id int PRIMARY KEY ,
   company_id int,
   name TEXT NOT NULL,
   assigned_candidates_cnt smallint NOT NULL
 );
 
 CREATE TABLE Company (
-  id serial PRIMARY KEY,
+  id int PRIMARY KEY,
   overview TEXT NOT NULL,
   type BOOLEAN NOT NULL,
   founded_on DATE NOT NULL,
   company_size INTEGER NOT NULL,
-  rating float NOT NULL,
+  rating smallint NOT NULL,
   name TEXT NOT NULL
 );
 
@@ -89,7 +89,8 @@ CREATE TABLE Candidates (
   phase_deadline TIMESTAMP,
   template_id INTEGER,
   placeholders_params JSON,
-  recruitment_process_id int NOT NULL
+  recruitment_process_id int NOT NULL,
+  PRIMARY KEY (job_id, seeker_id)
 );
 
 CREATE TABLE Job (
@@ -237,6 +238,8 @@ CREATE TABLE Recommendations (
 CREATE TABLE Recommendations_1_10000 PARTITION OF Recommendations FOR VALUES FROM (1) TO (10001);
 
 CREATE TABLE Reviews (
+  id serial PRIMARY KEY,
+  user_id int,
   company_id int NOT NULL,
   title text NOT NULL,
   description text NOT NULL,
@@ -296,9 +299,7 @@ CREATE INDEX ON Company_Location (company_id);
 
 CREATE INDEX candidates_recruiter_id_status_index ON Candidates (recruiter_id, phase);
 
-CREATE INDEX candidates_seeker_id_status_index ON Candidates (seeker_id, phase);
-
-CREATE INDEX candidates_job_id_status_index ON Candidates (job_id, phase);
+CREATE INDEX candidates_seeker_id_status_index ON Candidates (phase);
 
 CREATE INDEX candidates_last_status_update_index ON Candidates (last_status_update);
 
@@ -343,6 +344,10 @@ CREATE INDEX ON Logs (company_id, action_type);
 CREATE INDEX ON Logs (company_id, performed_by);
 
 CREATE INDEX ON Logs (company_id, created_at);
+
+CREATE INDEX ON Recruitment_Phase (type);
+
+ALTER TABLE Reviews ADD FOREIGN KEY (user_id) REFERENCES Job_Seeker (id) ON DELETE CASCADE;
 
 ALTER TABLE Candidate_History ADD FOREIGN KEY (seeker_id) REFERENCES Job_Seeker (id) ON DELETE CASCADE;
 
