c8d7dde63a8152fdc91f6f10bf5af2b0e5befb70
Ali <2021170338@cis.asu.edu.eg>
2025-02-21 01:04:04 +0200

    Add company routes and controller for company data retrieval; implement validation and service layer

--- Api Server/src/features/seekers/seekerRoutes.js
+++ Api Server/src/features/seekers/seekerRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const jobOfferRoutes = require('./job_offers/jobOfferRoutes');
+const companyRoutes = require('./companies/companyRoutes');
 
 router.use('/job-offers', jobOfferRoutes);
+
+router.use('/companies', companyRoutes);
 
 
 module.exports = router;
