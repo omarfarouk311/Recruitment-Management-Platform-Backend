a68f04ea8f7445b25d1397ae75080f4cae978cff
Abdelrahman034 <ar.salah403@gmail.com>
2025-02-21 14:02:20 +0200

    Implement transaction handling for review creation, update, and deletion; update company average rating accordingly

--- Api Server/src/features/reviews/reviewService.js
+++ Api Server/src/features/reviews/reviewService.js
@@ -8,29 +8,17 @@ exports.getReviews = async (companyId, filters) => {
 
 exports.createReview = async (reviewData) => {
     result = await reviewModel.createReview(reviewData);
-    await reviewModel.updateCompanyAvgRating(reviewData.companyId);
     return result;
 };
 
 exports.updateReview = async (reviewData) => {
 
     result = await reviewModel.updateReview(reviewData);
-    await reviewModel.updateCompanyAvgRating(reviewData.companyId);
     return result;
 };
 
 exports.deleteReview = async (reviewId) => {
-    // Get the review to retrieve the companyId
-    const review = await reviewModel.getReviewById(reviewId);
-    if (!review) {
-        throw new Error('Review not found');
-    }
-
-    // Delete the review
-    const result = await reviewModel.deleteReview(reviewId);
-
-    // Update the company's average rating
-    await reviewModel.updateCompanyAvgRating(review.companyId);
-
     
+    const result = await reviewModel.deleteReview(reviewId);
+    return result;
 };
