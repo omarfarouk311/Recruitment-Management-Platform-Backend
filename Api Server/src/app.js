ccee6f63c9cd5b7bc88b780ea73a461c9fb209d1
omaradwan <xomaradwanx@icloud.com>
2025-06-11 23:14:29 +0300

    modify recruiter data query to include assigned candidates count

--- Api Server/src/app.js
+++ Api Server/src/app.js
@@ -33,7 +33,7 @@ app.use(cors({ origin: '*' }));
 // for testing
 app.use((req, res, next) => {
     // console.log('request reached')
-    req.userId = 2;
+    req.userId = 3;
     req.userRole = role.recruiter;
     next();
 });
