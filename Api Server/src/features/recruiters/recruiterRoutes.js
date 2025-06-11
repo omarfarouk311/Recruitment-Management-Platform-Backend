75d81326e20e94210ea3d052157bf2c5aa3f20f7
Omar Farouk <omarfarouk219@gmail.com>
2025-06-11 19:01:25 +0300

    Refactor recruiter routes
    - Refactor recruiter route definitions to handle unsupported methods, ensuring proper error handling.
    - Add header validation for profile picture updates.

--- Api Server/src/features/recruiters/recruiterRoutes.js
+++ Api Server/src/features/recruiters/recruiterRoutes.js
@@ -1,68 +1,100 @@
-
 const express = require('express');
-const router=express();
-const {handleValidationErrors}=require('../../common/util')
-
-const recruiterController=require('./recruiterController');
-const {authorizeCompanyRecruiter,authorizeRecruiter,authorizeCompany}=require('./recruiterAuthorization')
-const {validateParams,validateRecruiterId,validateInvitationData,validateJobOffer,validateRecruiterName}=require('./recruiterValidation')
-
-
-router.route('/')   //localhost:3000/recruiters/?recruiter="" & department="" & sorted=""&page=""
-        .get(validateParams,
-            handleValidationErrors,
-            authorizeCompany,
-            recruiterController.getRecruitersContoller)
-
-router.route('/:recruiterId')  
-        .delete(validateRecruiterId,
-                handleValidationErrors,
-                authorizeCompany,
-                authorizeCompanyRecruiter,
-                recruiterController.deleteRecruiterController)
+const router = express();
+const { handleValidationErrors } = require('../../common/util');
+const { notAllowed } = require('../../common/errorMiddleware');
+const { validateFileNameHeader } = require('../../common/util');
+const recruiterController = require('./recruiterController');
+const { authorizeCompanyRecruiter, authorizeRecruiter, authorizeCompany } = require('./recruiterAuthorization');
+const { validateParams, validateRecruiterId, validateJobOffer, validateRecruiterName } = require('./recruiterValidation');
 
 router.route('/departments')
-        .get(authorizeCompany,
-             recruiterController.getUniquetDepartmentsController)
-
+    .get(
+        authorizeCompany,
+        recruiterController.getUniquetDepartmentsController
+    )
+    .all(notAllowed);
 
 router.route('/job-offer-sent')
-        .get(validateJobOffer,
-            handleValidationErrors,
-            authorizeRecruiter,
-            recruiterController.getJobOfferSentController)
+    .get(
+        validateJobOffer,
+        handleValidationErrors,
+        authorizeRecruiter,
+        recruiterController.getJobOfferSentController
+    )
+    .all(notAllowed);
 
 router.route('/assigned-Candidate-JobTitles')  // get list of the jobs the recruiter see  because of the candidate he assigned to  
-        .get(authorizeRecruiter,
-             recruiterController.getJobTitleList)
+    .get(
+        authorizeRecruiter,
+        recruiterController.getJobTitleList
+    )
+    .all(notAllowed);
 
 router.route('/profile-data')
-        .get(authorizeCompany,
-             recruiterController.getRecruiterDataController)
-
-router.route('/:userId/profile-pic')
-        .get(authorizeRecruiter
-            ,recruiterController.getProfilePicController)
+    .get(
+        authorizeCompany,
+        recruiterController.getRecruiterDataController
+    )
+    .all(notAllowed);
 
 router.route('/profile-pic')
-        .put(authorizeRecruiter,
-                recruiterController.updateProfilePicController)
-
-
+    .put(
+        authorizeRecruiter,
+        validateFileNameHeader(),
+        handleValidationErrors,
+        recruiterController.updateProfilePicController
+    )
+    .all(notAllowed);
 
 router.route('/allRecruiters')
-        .get(authorizeCompany,
-            recruiterController.getAllRecruitersController)
-
+    .get(
+        authorizeCompany,
+        recruiterController.getAllRecruitersController
+    )
+    .all(notAllowed);
 
 router.route('/recruiter')
-        .put(authorizeRecruiter,
-             validateRecruiterName,
-             handleValidationErrors,
-            recruiterController.updateRecruiterController)
-
+    .put(
+        authorizeRecruiter,
+        validateRecruiterName,
+        handleValidationErrors,
+        recruiterController.updateRecruiterController
+    )
+    .all(notAllowed);
 
 router.route('/finish-profile')
-        .post(validateRecruiterName,
-            recruiterController.finishProfileController)
-module.exports=router
+    .post(
+        authorizeRecruiter,
+        validateRecruiterName,
+        recruiterController.finishProfileController
+    )
+    .all(notAllowed);
+
+router.route('/:userId/profile-pic')
+    .get(
+        authorizeRecruiter,
+        recruiterController.getProfilePicController
+    )
+    .all(notAllowed);
+
+router.route('/:recruiterId')
+    .delete(
+        validateRecruiterId,
+        handleValidationErrors,
+        authorizeCompany,
+        authorizeCompanyRecruiter,
+        recruiterController.deleteRecruiterController
+    )
+    .all(notAllowed);
+
+router.route('/')   //localhost:3000/recruiters/?recruiter="" & department="" & sorted=""&page=""
+    .get(
+        validateParams,
+        handleValidationErrors,
+        authorizeCompany,
+        recruiterController.getRecruitersContoller
+    )
+    .all(notAllowed);
+
+
+module.exports = router;
