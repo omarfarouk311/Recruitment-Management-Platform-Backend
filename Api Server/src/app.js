fcc006181405cc76b396cfec6f532326468be5b7
Amr <zekabeka420@yahoo.com>
2025-06-29 04:43:47 +0300

    Update front end files served from nginx and disabled console.log in app.js

--- Api Server/src/app.js
+++ Api Server/src/app.js
@@ -1,3 +1,4 @@
+console.log = () => { };
 process.env.TZ = 'UTC';
 const express = require('express');
 const { port } = require('../config/config');
@@ -21,10 +22,10 @@ const cvRoutes = require('./features/cvs/cvRoutes');
 const authRoutes = require('./features/auth/authRoutes');
 const skillsRoutes = require('./features/skills/skillsRoutes');
 const { authenticateUser } = require('./features/auth/authController');
-const cors = require('cors');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const app = express();
+
 minioConnect();
 
 app.use(helmet());
@@ -32,7 +33,7 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
-app.use(authenticateUser); // this middleware will authenticate the user for all the routes below it
+app.use(authenticateUser); // authenticates the user for all the routes below it
 
 app.use('/api/cvs', cvRoutes); // parses the cv when the user uploads it
 app.use('/api/assessments', assessmentRoutes);
@@ -56,5 +57,5 @@ app.use(notFound);
 app.use(errorHandlingMiddleware);
 
 app.listen(port, () => {
-    console.log('The server is running and listening on port ${port}');
+    console.log(`The server is running and listening on port ${port}`);
 });
